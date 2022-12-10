187caf13d63bec8abc5b65bb0188b6de3e4065fb
Corey <cschanz3939@gmail.com>
2022-12-09 19:53:11 -0600

    navbar

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -1,68 +1,40 @@
 import { NavLink } from 'react-router-dom';
-import './Nav.css'
+//import './Nav.css'
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-success">
-      <div className="container-fluid">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <NavLink className="navbar-brand" to="/">CarCar</NavLink>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        {/* <div className="collapse navbar-collapse" id="navbarSupportedContent"> */}
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-              <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
-          </li>
 
-          <li className="nav-item dropdown">
-          <NavLink className="nav-link dropdown-toggle" to="/service/new" id="navbarDropdownMenuLink" role="button"
-          data-mdb-toggle="dropdown" aria-expanded="false">
-          Service
-        </NavLink>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li>
-              <NavLink className="dropdown-item" to="/service/new">Service Appointment</NavLink>
-            </li>
-            <li>
-              <NavLink className="dropdown-item" to="/service">Service List</NavLink>
-            </li>
-            </ul>
-          </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/customers/new">New Customers</NavLink>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <NavLink className="nav-link" to="">Home <span className="sr-only">(current)</span></NavLink>
             </li>
             <li className="nav-item">
               <NavLink className="nav-link" to="/manufacturers/">Manufacturers</NavLink>
             </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/manufacturers/new">New Manufacturers</NavLink>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropdown
+              </a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="#">Action</a>
+                <a className="dropdown-item" href="#">Another action</a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="#">Something else here</a>
+              </div>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/models/">Models</NavLink>
+              <a className="nav-link disabled" href="#">Disabled</a>
             </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/models/new">New Model</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/automobiles/">Automobile</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/automobiles/new">New Automobile</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/sales_persons/new">New Sales person</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/sales_records/new">New Sales Records</NavLink>
-            </li>
-            {/* <li className="nav-item">
-              <NavLink className="nav-link" to="/inventory_list">Inventory List</NavLink>
-            </li> */}
           </ul>
         </div>
-      {/* </div> */}
-    </nav>
+      </nav>
+
   )
 }
 
