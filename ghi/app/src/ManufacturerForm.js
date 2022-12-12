92a01ecd5114060aa23bd8802472e4800760dec7
Gina <gejohn793@gmail.com>
2022-12-12 14:59:44 -0600

    Final push!

--- ghi/app/src/ManufacturerForm.js
+++ ghi/app/src/ManufacturerForm.js
@@ -5,7 +5,6 @@ class ManufacturerForm extends React.Component {
         name: '',
     };
 
-
   handleSubmit = async (event) => {
     event.preventDefault();
     const data = {...this.state};
@@ -15,9 +14,7 @@ class ManufacturerForm extends React.Component {
       method: "post",
       body: JSON.stringify(data),
       headers: {
-        'Content-Type': 'application/json',
-      },
-    };
+        'Content-Type': 'application/json'}};
     const response = await fetch(manufacturerUrl, fetchConfig);
     if (response.ok) {
       const newManufacturer = await response.json();
@@ -32,30 +29,28 @@ class ManufacturerForm extends React.Component {
       console.log("an error has occured")
     }
   }
+
   handleNameChange = (event) => {
     const value = event.target.value;
-    this.setState({name: value})
-}
-    render() {
-        return (
+    this.setState({name: value})}
 
-        <div className="row">
-        <div className="offset-3 col-6">
-          <div className="shadow p-4 mt-4">
-            <h1>Create a new manufacturer</h1>
-            <form onSubmit={this.handleSubmit} id="create-hat-form">
-              <div className="form-floating mb-3">
-                <input onChange={this.handleNameChange} placeholder="Name" required type="text" name="name" id="id" className="form-control" value={this.state.name}/>
-                <label htmlFor="name">Name</label>
-                </div>
-              <button className="btn btn-primary">Create</button>
-            </form>
-          </div>
-        </div>
+render() {
+    return (
+
+    <div className="row">
+    <div className="offset-3 col-6">
+      <div className="shadow p-4 mt-4">
+        <h1>Create a new manufacturer</h1>
+        <form onSubmit={this.handleSubmit} id="create-hat-form">
+          <div className="form-floating mb-3">
+            <input onChange={this.handleNameChange} placeholder="Name" required type="text" name="name" id="id" className="form-control" value={this.state.name}/>
+            <label htmlFor="name">Name</label>
+            </div>
+          <button className="btn btn-primary">Create</button>
+        </form>
       </div>
-
-        );
-    }
-}
+    </div>
+  </div>
+)}}
 
 export default ManufacturerForm
